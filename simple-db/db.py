6da0829d2af68f53898e15ac3b808b55cdfc486f
JObog <jobogbaimhe@gmail.com>
2021-10-06 16:12:26 -0400

    added DB class methods

---
+++ simple-db/db.py
@@ -0,0 +1,17 @@
+import json 
+
+class DB:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def add(self, obj):
+        pass
+
+    def get(self, fn=None):
+        pass
+
+    def set(self, fn=None):
+        pass
+
+    def remove(self, fn=None):
+        pass
